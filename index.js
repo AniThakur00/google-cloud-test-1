1f656f74d1f25ff682bb130dae5c68ec78bf409e
AniThakur00 <ani1998.at@gmail.com>
2023-10-01 17:48:19 -0400

    test-4.0.0

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json())
 
 app.all('*', (req, res) => {
     console.log(req.path)
-    res.send('Hello, World from app1! Version:1 Test-3');
+    res.send('Hello, World from app1! Version:1 Test-4');
 });
 app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
