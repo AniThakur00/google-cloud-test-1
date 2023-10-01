09e287c97789d962a71664c00481d5b01c4c7d7d
AniThakur00 <ani1998.at@gmail.com>
2023-10-01 15:20:19 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+const port = process.env.PORT || 8000;
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger_output.json')
+
+app.use(bodyParser.json())
+
+
+app.all('*', (req, res) => {
+    console.log(req.path)
+    res.send('Hello, World from app1! Version:1 Test-1');
+});
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
+app.post('/order', (req, res) => {
+
+    let x = 'hi'
+
+    res.json({ message: x, body: req.body })
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+});
